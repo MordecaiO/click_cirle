6a7482c0462840b9ccb8ea5baf7834273d83455f
mordecaioladimeji <cai.oladimeji@gmail.com>
2022-11-28 07:02:21 +0000

    adding state to store clicked points

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [points, setPoints] = useState([]);
+
   const handlePlaceCircle = (e) => {
     console.log("you just clicked");
+    const { clientX, clientY } = e;
+
+    setPoints([
+      ...points,
+      {
+        x: clientX,
+        y: clientY,
+      },
+    ]);
   };
+
   return <div className="App" onClick={handlePlaceCircle}></div>;
 }
 
