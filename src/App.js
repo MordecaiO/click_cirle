244c00a06b936dcf690357809300f20dc499ae6f
mordecaioladimeji <cai.oladimeji@gmail.com>
2022-11-28 08:01:20 +0000

    adding undo button

--- src/App.js
+++ src/App.js
@@ -16,20 +16,29 @@ function App() {
       },
     ]);
   };
-
+  const handleUndo = () => {
+    //shallow copy of current points
+    let newPoints = [...points];
+    // remove last added point
+    newPoints.pop();
+    setPoints(newPoints);
+  };
   return (
-    <div className="App" onClick={handlePlaceCircle}>
-      {points.map((point) => {
-        return (
-          <div
-            className="point"
-            style={{ top: point.y + "px", left: point.x + "px" }}
-          >
-            x
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <button className="undo" onClick={handleUndo}>
+        Undo
+      </button>
+      <div className="App" onClick={handlePlaceCircle}>
+        {points.map((point) => {
+          return (
+            <div
+              className="point"
+              style={{ top: point.y + "px", left: point.x + "px" }}
+            ></div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 export default App;
